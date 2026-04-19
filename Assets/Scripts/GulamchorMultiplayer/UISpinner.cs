9294fc83afc07e1231a9867940f18610a203e542
Sumiran Bohara <boharasumiran@gmail.com>
2026-04-19 16:22:20 +0545

    Multiplayer lobby and multiplayer refinement

---
+++ Assets/Scripts/GulamchorMultiplayer/UISpinner.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+public class UISpinner : MonoBehaviour
+{
+    public float speed = 200f;
+    void Update()
+    {
+        transform.Rotate(0f, 0f, -speed * Time.deltaTime);
+    }
+}
